3c0d5d79634b5c9c11a9e2f79452391783beea2b
GHOULS <69440954+cryptoghouls@users.noreply.github.com>
2021-07-25 12:46:17 -0700

    Add files via upload

---
+++ precache-manifest.0e9b58916989a476bf03f8e517ae41e5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b05fa1a479530bfb95dc3125436b278",
+    "url": "./index.html"
+  },
+  {
+    "revision": "418a49be6f102e584bf7",
+    "url": "./static/css/main.bff1206c.chunk.css"
+  },
+  {
+    "revision": "04f0f8e1f35931674434",
+    "url": "./static/js/0.d5c2ade1.chunk.js"
+  },
+  {
+    "revision": "cdbbaff6545208bdea8b",
+    "url": "./static/js/10.4be99772.chunk.js"
+  },
+  {
+    "revision": "ef998acd8f65ae6b9b420793d7484b0e",
+    "url": "./static/js/10.4be99772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d78a2918d000256c9d",
+    "url": "./static/js/11.8c85c96f.chunk.js"
+  },
+  {
+    "revision": "4e5893e16524b71427cb",
+    "url": "./static/js/12.d13b00be.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/12.d13b00be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04c7716ddd9dbdc764b",
+    "url": "./static/js/13.f2e916c3.chunk.js"
+  },
+  {
+    "revision": "f9d495cddd9bc7373c5f",
+    "url": "./static/js/14.8821814c.chunk.js"
+  },
+  {
+    "revision": "ddcb66487065c7fa75f2",
+    "url": "./static/js/3.beb8fce7.chunk.js"
+  },
+  {
+    "revision": "de92679a7a27415208d79c3b4394480f",
+    "url": "./static/js/3.beb8fce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3abfba9e8b8c3c6cba",
+    "url": "./static/js/4.0b746f5a.chunk.js"
+  },
+  {
+    "revision": "655d3b6d19a6f311ebf8edac1ed145e6",
+    "url": "./static/js/4.0b746f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4579bdd5716fa84a7ffc",
+    "url": "./static/js/5.b0168389.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/5.b0168389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1548810d5b76b14b9d",
+    "url": "./static/js/6.c727f1f6.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "./static/js/6.c727f1f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9ff1a5042d3bd72d49",
+    "url": "./static/js/7.2531e8a6.chunk.js"
+  },
+  {
+    "revision": "2872e62464276fc01072",
+    "url": "./static/js/8.dcffc570.chunk.js"
+  },
+  {
+    "revision": "932213ff9350889c4ac441758e5e82b6",
+    "url": "./static/js/8.dcffc570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf128ecef408050467b",
+    "url": "./static/js/9.c9f32a16.chunk.js"
+  },
+  {
+    "revision": "418a49be6f102e584bf7",
+    "url": "./static/js/main.b99e6c17.chunk.js"
+  },
+  {
+    "revision": "6d2287ca5c810cd556ec",
+    "url": "./static/js/runtime-main.2665f30f.js"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "./static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "62578f5994645a1825d4829e2c4394b0",
+    "url": "./static/media/coinbaseWalletIcon.62578f59.svg"
+  },
+  {
+    "revision": "54d70e4e51ca31f8b8d1eb162851dd5f",
+    "url": "./static/media/icn-aethereum.54d70e4e.svg"
+  },
+  {
+    "revision": "62825e60a8dc908aef6082461f302133",
+    "url": "./static/media/icn-ledger.62825e60.svg"
+  },
+  {
+    "revision": "e8109252c38f5f7874506c6731c52292",
+    "url": "./static/media/icn-metamask.e8109252.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "./static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "./static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "./static/media/walletConnectIcon.8215855c.svg"
+  }
+]);
